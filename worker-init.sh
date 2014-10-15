f41bca78baf17f739765986348ee6d2b56a85680
Chris Froussios <darkmatter.tm@gmail.com>
2014-10-15 14:03:53 +0200

    Worker script creates a deamon

--- worker-init.sh
+++ worker-init.sh
@@ -1,11 +1,17 @@
 #!/bin/bash
 
+#Step 1: Go to familiar places
 cd /home/ec2-user/
 
+#Step 2: Leave traces that you were there
 echo Its-a me >> Mario.txt
 
+#Step 3: Get someone more important than you
 wget https://www.dropbox.com/s/0m7tbj0xj4ht1gh/dummy.sh?dl=0 -O dummy.sh
+awk '{ sub("\r$", ""); print }' dummy.sh > dummy.sh.fix
+mv dummy.sh.fix dummy.sh
 chmod +x dummy.sh
 
-/bin/bash dummy.sh
+#Step 4: Execute said someone
+nohup /bin/bash dummy.sh 0<&- &> /home/ec2-user/deamon.log &
 
