1b76a7f5d0c78a5e60f0fb9d38c4c422f38a0096
Chris Froussios <Froussios@users.noreply.github.com>
2014-10-23 15:25:05 +0200

    Updated to load dynamic part from github

--- worker-init.sh
+++ worker-init.sh
@@ -1,19 +1,30 @@
 #!/bin/bash
 
+# This script exists on every worker instance.
+# It automatically loads another script.
+# Update that script to change worker initialisation behaviour.
+
+LOG="init.log"
+DYNAMIC="https://raw.githubusercontent.com/rvanheest/IN4392_Cloud_Computing_Labs/master/worker-dynamic.sh"
+
+
 #Step 1: Go to familiar places
 rm -r /home/ubuntu/worker-workspace/
 mkdir /home/ubuntu/worker-workspace/
 cd /home/ubuntu/worker-workspace/
 
 #Step 2: Leave traces that you were there
-echo Its-a me 123 >> Mario.txt
+echo worker-init.sh: The workspace has been initialized >> $LOG
 
 #Step 3: Get someone more important than you
-wget https://www.dropbox.com/s/0m7tbj0xj4ht1gh/dummy.sh?dl=0 -O dummy.sh
-awk '{ sub("\r$", ""); print }' dummy.sh > dummy.sh.fix
-mv dummy.sh.fix dummy.sh
-chmod +x dummy.sh
+wget $DYNAMIC -O dynamic.sh
+awk '{ sub("\r$", ""); print }' dynamic.sh > dynamic.sh.fix
+mv dynamic.sh.fix dynamic.sh
+chmod +x dynamic.sh
+echo worker-init.sh: Dynamic part of script download >> $LOG
 
 #Step 4: Execute said someone
-nohup /bin/bash dummy.sh 0<&- &> /home/ubuntu/deamon.log &
+#nohup /bin/bash dynamic.sh 0<&- &> /home/ubuntu/deamon.log &
+/bin/bash dynamic.sh
+echo worker-init.sh: Dynamic part of script executed >> $LOG
 
