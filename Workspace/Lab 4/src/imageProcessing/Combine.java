d1c2bc48b8b8455b78ade7b7d1287a93521b34e8
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2014-10-23 10:29:32 +0200

    Fixed combine to process color correctly

--- Workspace/Lab 4/src/imageProcessing/Combine.java
+++ Workspace/Lab 4/src/imageProcessing/Combine.java
@@ -1,5 +1,6 @@
 package imageProcessing;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.util.concurrent.Callable;
 
@@ -29,11 +30,15 @@ public class Combine implements Callable<BufferedImage> {
 
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
-				int px1 = this.input1.getRGB(x, y);
-				int px2 = this.input2.getRGB(x, y);
-
-				double combined = px1 * this.part + px2 * (1 - this.part);
-				dest.setRGB(x, y, (int) combined);
+				Color c1 = new Color(this.input1.getRGB(x, y));
+				Color c2 = new Color(this.input2.getRGB(x, y));
+				
+				int red = (int) (c1.getRed() * this.part + c2.getRed() * (1 - this.part));
+				int green = (int) (c1.getGreen() * this.part + c2.getGreen() * (1 - this.part));
+				int blue = (int) (c1.getBlue() * this.part + c2.getBlue() * (1 - this.part));
+				
+				Color combined = new Color(red, green, blue);
+				dest.setRGB(x, y, combined.getRGB());
 			}
 		}
 
--- Workspace/Lab 4/src/imageProcessing/Test.java
+++ Workspace/Lab 4/src/imageProcessing/Test.java
@@ -9,17 +9,17 @@ import javax.imageio.ImageIO;
 public class Test {
 
 	public static void main(String[] args) throws IOException {
-		BufferedImage image = ImageIO.read(new File("testimg/Apen.JPG"));
+		BufferedImage image = ImageIO.read(new File("testimg/Eiger.JPG"));
 		
 		long t1 = System.currentTimeMillis();
-		BufferedImage gray = new GrayScaling(image).call();
+//		BufferedImage gray = new GrayScaling(image).call();
 //		BufferedImage noise = new Noise(image, 10, 200).call();
 //		BufferedImage invert = new Invert(image).call();
 //		BufferedImage burn = new Burn(image).call();
 //		BufferedImage gaus = new Gaussian(image).call();
 //		BufferedImage comb = new Combine(gray, gaus, 0.9).call();
-		BufferedImage flipV = new FlipVertical(gray).call();
-		BufferedImage flipH = new FlipHorizontal(gray).call();
+		BufferedImage flipV = new FlipVertical(image).call();
+		BufferedImage flipH = new FlipHorizontal(image).call();
 		BufferedImage comb = new Combine(flipV, flipH, 0.5).call();
 		
 		System.out.println(System.currentTimeMillis() - t1);
