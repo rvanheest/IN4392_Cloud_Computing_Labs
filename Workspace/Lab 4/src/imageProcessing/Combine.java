0c2a71afa2d1050bc93a710dc30898dc2e5df048
Richard van Heest <A.w.m.vanHeest@student.tudelft.nl>
2014-10-23 10:46:20 +0200

    Making the Combine recursive

--- Workspace/Lab 4/src/imageProcessing/Combine.java
+++ Workspace/Lab 4/src/imageProcessing/Combine.java
@@ -6,32 +6,34 @@ import java.util.concurrent.Callable;
 
 public class Combine implements Callable<BufferedImage> {
 
-	private final BufferedImage input1;
-	private final BufferedImage input2;
+	private final Callable<BufferedImage> input1;
+	private final Callable<BufferedImage> input2;
 	private final double part;
 
-	public Combine(BufferedImage input1, BufferedImage input2, double part) {
-		if (input1.getWidth() == input2.getWidth() && input1.getHeight() == input2.getHeight()) {
-    		this.input1 = input1;
-    		this.input2 = input2;
-    		this.part = part;
-		}
-		else {
-			throw new IllegalArgumentException("dimensions aren't the same!");
-		}
+	public Combine(Callable<BufferedImage> input1, Callable<BufferedImage> input2, double part) {
+		this.input1 = input1;
+		this.input2 = input2;
+		this.part = part;
+	}
+
+	public Combine(Callable<BufferedImage> input1, Callable<BufferedImage> input2) {
+		this(input1, input2, 0.5);
 	}
 
 	@Override
-	public BufferedImage call() {
-		int width = this.input1.getWidth();
-		int height = this.input1.getHeight();
+	public BufferedImage call() throws Exception {
+		BufferedImage im1 = this.input1.call();
+		BufferedImage im2 = this.input2.call();
+		
+		int width = im1.getWidth();
+		int height = im1.getHeight();
 
 		BufferedImage dest = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
 
 		for (int x = 0; x < width; x++) {
 			for (int y = 0; y < height; y++) {
-				Color c1 = new Color(this.input1.getRGB(x, y));
-				Color c2 = new Color(this.input2.getRGB(x, y));
+				Color c1 = new Color(im1.getRGB(x, y));
+				Color c2 = new Color(im2.getRGB(x, y));
 				
 				int red = (int) (c1.getRed() * this.part + c2.getRed() * (1 - this.part));
 				int green = (int) (c1.getGreen() * this.part + c2.getGreen() * (1 - this.part));
--- Workspace/Lab 4/src/imageProcessing/Test.java
+++ Workspace/Lab 4/src/imageProcessing/Test.java
@@ -2,25 +2,31 @@ package imageProcessing;
 
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
+import java.util.concurrent.Callable;
 
 import javax.imageio.ImageIO;
 
 public class Test {
 
-	public static void main(String[] args) throws IOException {
+	public static void main(String[] args) throws Exception {
 		BufferedImage image = ImageIO.read(new File("testimg/Eiger.JPG"));
-		
+
+		Callable<BufferedImage> gray = new GrayScaling(image);
+		Callable<BufferedImage> noise = new Noise(image, 10, 200);
+		Callable<BufferedImage> invert = new Invert(image);
+		Callable<BufferedImage> burn = new Burn(image);
+		Callable<BufferedImage> gaus = new Gaussian(image);
+		Callable<BufferedImage> flipV = new FlipVertical(image);
+		Callable<BufferedImage> flipH = new FlipHorizontal(image);
+
+		Combine c1 = new Combine(gray, noise);
+		Combine c2 = new Combine(invert, burn);
+		Combine c3 = new Combine(c1, flipV);
+		Combine c4 = new Combine(c2, flipH);
+		Combine c5 = new Combine(c3, c4);
+
 		long t1 = System.currentTimeMillis();
-//		BufferedImage gray = new GrayScaling(image).call();
-//		BufferedImage noise = new Noise(image, 10, 200).call();
-//		BufferedImage invert = new Invert(image).call();
-//		BufferedImage burn = new Burn(image).call();
-//		BufferedImage gaus = new Gaussian(image).call();
-//		BufferedImage comb = new Combine(gray, gaus, 0.9).call();
-		BufferedImage flipV = new FlipVertical(image).call();
-		BufferedImage flipH = new FlipHorizontal(image).call();
-		BufferedImage comb = new Combine(flipV, flipH, 0.5).call();
+		BufferedImage comb = new Combine(c5, gaus, 0.8).call();
 		
 		System.out.println(System.currentTimeMillis() - t1);
 		
