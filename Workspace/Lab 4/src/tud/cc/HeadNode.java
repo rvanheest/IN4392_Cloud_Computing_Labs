dce483e9f85ff95a4d85f655bfc25dc340db51eb
Chris Froussios <darkmatter.tm@gmail.com>
2014-10-31 00:49:28 +0100

    Thread blueprint to separate file

--- Workspace/Lab 4/src/tud/cc/HeadNode.java
+++ Workspace/Lab 4/src/tud/cc/HeadNode.java
@@ -28,22 +28,6 @@ import data.Task;
 import emulator.Emulator;
 
 
-abstract class CloseableThread
-	extends Thread
-	implements AutoCloseable
-{
-	public CloseableThread()
-	{
-	}
-	
-	public CloseableThread(String name)
-	{
-		super("[T-" + name + "]");
-	}
-}
-
-
-
 class Connection
 	implements AutoCloseable
 {
