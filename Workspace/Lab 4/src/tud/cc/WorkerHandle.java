03dd2ed8637dae4d8a4be954094507c1c10fa743
Chris Froussios <darkmatter.tm@gmail.com>
2014-11-01 00:55:03 +0100

    WorkerHandle tracks worker load

--- Workspace/Lab 4/src/tud/cc/WorkerHandle.java
+++ Workspace/Lab 4/src/tud/cc/WorkerHandle.java
@@ -4,8 +4,7 @@ import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
-import java.util.Map;
-import java.util.Queue;
+import java.util.*;
 
 import tud.cc.WorkerHandle;
 import amazonTests.NodeDetails;
@@ -26,6 +25,12 @@ public class WorkerHandle
 	private final ObjectOutputStream out;
 	private final Queue<Task> processedQueue;
 	private final Map<String, WorkerHandle> workerPool;
+	
+	/**
+	 * Contains the jobs currently being handled by this worker
+	 * as a set of (jobID -> imageSize) tuples
+	 */
+	private final Map<UUID, Integer> jobsInProcess = Collections.synchronizedMap(new HashMap<UUID, Integer>());
 	
 	
 	public WorkerHandle(NodeDetails nodeDetails, Socket workerSocket, Queue<Task> processedQueue, Map<String, WorkerHandle> workerPool) throws IOException
@@ -56,6 +61,7 @@ public class WorkerHandle
 				Task job = (Task) in.readObject();
 				System.out.println(getName() + ": received from worker: " + job.getImage().length + "b");
 				job.processed();
+				this.jobsInProcess.remove(job.getUuid());
 				
 				// Queue response to client
 				processedQueue.add(job);
@@ -69,18 +75,40 @@ public class WorkerHandle
 	}
 	
 	
+	/**
+	 * Get a snapshot of the workers current workload.
+	 * The data stop being up-to-date as soon as the function returns.
+	 * @return The snapshot
+	 */
+	public Map<UUID, Integer> getWorkloadSnapshot()
+	{
+		return new HashMap<UUID, Integer>(this.jobsInProcess);
+	}
+	
+	
+	/**
+	 * Send a job to be executed by this worker.
+	 * This function blocks on the calling thread.
+	 * @param job The job to sent.
+	 * @throws IOException The connection to the worker has failed.
+	 */
 	public synchronized void sendJob(Task job) throws IOException
 	{
 		if (job == null)
 			throw new NullPointerException("Job cannot be null");
 		
-		System.out.println("Sending job to " + workerSocket.getInetAddress().getHostAddress());
+		System.out.println(Thread.currentThread().getName() +  " sending job to " + workerSocket.getInetAddress().getHostAddress());
+
+		this.jobsInProcess.put(job.getUuid(), job.getImage().length);
 		
 		// Send job
 		out.writeObject(job);
 	}
 	
 	
+	/**
+	 * Release the previously leased EC2 instance
+	 */
 	public void release() 
 	{
 		System.out.println("Releasing node " + nodeDetails.getNodeAddress().getHostAddress());
@@ -88,6 +116,9 @@ public class WorkerHandle
 	}
 	
 
+	/**
+	 * Close socket, remove worker from local collections and release the machine.
+	 */
 	@Override
 	public void close() throws Exception 
 	{
@@ -104,4 +135,17 @@ public class WorkerHandle
 		
 		System.out.println(getName() + " closed.");
 	}
+	
+	
+	@Override
+	public String toString() 
+	{
+		int bs = 0;
+		for (Integer i : this.jobsInProcess.values())
+			bs += i;
+		
+		return this.workerSocket.getInetAddress().getHostAddress() + ": " + this.jobsInProcess.size() + " jobs, " + bs + " bytes";
+	}
 }
+
+
