613fa6a118bd23d71a64f339a11adeb61f743599
Chris Froussios <darkmatter.tm@gmail.com>
2014-11-06 11:55:40 +0100

    Added import

--- Workspace/Lab 4/src/tud/cc/HeadNode.java
+++ Workspace/Lab 4/src/tud/cc/HeadNode.java
@@ -10,6 +10,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.Deque;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
 import java.util.Map.Entry;
 import java.util.UUID;
