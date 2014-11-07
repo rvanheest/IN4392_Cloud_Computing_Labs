c625aa9eef9ca9b4a87da7d1269badddc7e38d33
Chris Froussios <darkmatter.tm@gmail.com>
2014-11-07 14:47:28 +0100

    Removed obsolete conditions

--- Workspace/Lab 4/src/tud/cc/MonitorThread.java
+++ Workspace/Lab 4/src/tud/cc/MonitorThread.java
@@ -55,31 +55,14 @@ public class MonitorThread
 		if (workers.size() == 0)
 			cond1 = true;
 		
-		// Condition 2: job queue exceeds 10 jobs for 5 consecutive samples
-//		boolean cond2 = false;
-//		int over = 0;
-//		for (Sample sample : this.samples.subList(Math.max(samples.size()-5,0), samples.size()))
-//			if (sample.queueSize > 10)
-//				over++;
-//		if (over > 10)
-//			cond2 = true;
-		
-		// Condition 3: each worker has more than 4 jobs
-//		boolean cond3 = true;
-//		for (WorkerHandle worker : workers)
-//			if (worker.getJobsInProcess().size() < 4)
-//				cond3 = false;
-		
-		// Condition 4: workload over 80%
-		boolean cond4 = true;
-		cond4 = samples.getLast().getSmoothPromisedWorkload() > 0.8;
+		// Condition 2: workload over 80%
+		boolean cond2 = true;
+		cond2 = samples.getLast().getSmoothPromisedWorkload() > 0.8;
 		
 		
 		return new Boolean[] {
 				cond1,
-//				cond2,
-//				cond3,
-				cond4
+				cond2
 		};
 	}
 	
@@ -92,24 +75,14 @@ public class MonitorThread
 	{
 		Collection<WorkerHandle> workers = getWorkers();
 		
-		// Condition 1: if three workers have no work
-//		boolean cond1 = false;
-//		int idle = 0;
-//		for (WorkerHandle worker : workers)
-//			if (worker.getJobsInProcess().size() == 0)
-//				idle++;
-//		if (idle >= 3)
-//			cond1 = true;
-		
-		// Condition 2: Workload below 50% (more than 2 workers)
-		boolean cond2 = false;
-		cond2 = samples.getLast().getSmoothPromisedWorkload() < 0.5
+		// Condition 1: Workload below 50% (more than 2 workers)
+		boolean cond1 = false;
+		cond1 = samples.getLast().getSmoothPromisedWorkload() < 0.5
 				&& workers.size() > 2;
 		
 		
 		return new Boolean[] {
-//				cond1,
-				cond2
+				cond1
 		};
 	}
 	
